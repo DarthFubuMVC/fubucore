05cb533b1c7ffd2801cf147c864d152e7296d0f9
Chad Myers <chad.myers@gmail.com>
2011-06-07 13:57:12 -0500

    Converted some comments to API DOC XML comments

--- src/FubuCore/ObjectConverter.cs
+++ src/FubuCore/ObjectConverter.cs
@@ -3,20 +3,34 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text.RegularExpressions;
-using FubuCore.Binding;
 using FubuCore.Util;
 
 namespace FubuCore
 {
     public interface IObjectConverter
     {
-        // Given a string and a .Net type, read this string
-        // and give me back a corresponding instance of that
-        // type
+        /// <summary>
+        /// Given a string and a .Net type, read this string
+        /// and give me back a corresponding instance of that
+        /// type
+        /// </summary>
+        /// <param name="stringValue">The value to convert</param>
+        /// <param name="type">The desired destination type</param>
+        /// <returns>The value converted into the specified desination type</returns>
         object FromString(string stringValue, Type type);
+        /// <summary>
+        /// Given a string and a .Net type, T, read this string
+        /// and give me back a corresponding instance of type T.
+        /// </summary>
+        /// <typeparam name="T">The desired destination type</typeparam>
+        /// <param name="stringValue">The value to convert</param>
+        /// <returns>The value converted into the specified desination type</returns>
         T FromString<T>(string stringValue);
-
-
+        /// <summary>
+        /// Determines whether there is conversion support registered for the specified type
+        /// </summary>
+        /// <param name="type">The desired destination type</param>
+        /// <returns>True if conversion to this type is supported, otherwise False.</returns>
         bool CanBeParsed(Type type);
     }
 
