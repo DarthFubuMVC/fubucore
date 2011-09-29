e8fd7a6ed355ecc3750824ec88259f078877ae41
Dru Sellers <dru@drusellers.com>
2011-09-29 11:08:50 -0500

    putting 'IfNotNull' extensions back in

---
+++ src/FubuCore.Testing/BasicExtensionsTester.cs
@@ -0,0 +1,52 @@
+﻿using FubuTestingSupport;
+using NUnit.Framework;
+
+namespace FubuCore.Testing
+{
+    [TestFixture]
+    public class BasicExtensionsTester
+    {
+         
+        [Test]
+        public void should_be_able_to_handle_null_targets()
+        {
+            Tester t = null;
+            t.IfNotNull(te => te.StringValue).ShouldEqual(null);
+
+        }
+
+        [Test]
+        public void should_be_able_to_handle_value_types()
+        {
+            Tester t = null;
+            t.IfNotNull(te => te.Value).ShouldEqual(0);
+
+        }
+
+        [Test]
+        public void should_not_call_on_null_objects()
+        {
+            Tester t = null;
+            t.CallIfNotNull(te => te.Call());
+        }
+
+        [Test]
+        public void should_call_on_instantiated_objects()
+        {
+            var t = new Tester();
+            t.CallIfNotNull(te => te.Call());
+            t.Called.ShouldBeTrue();
+        }
+
+        class Tester
+        {
+            public bool Called;
+            public int Value { get; set; }
+            public string StringValue { get; set; }
+            public void Call()
+            {
+                Called = true;
+            }
+        }
+    }
+}
--- src/FubuCore/BasicExtensions.cs
+++ src/FubuCore/BasicExtensions.cs
@@ -15,5 +15,26 @@ namespace FubuCore
                 // That's right, swallow that exception
             }
         }
+
+        public static TOut IfNotNull<TTarget, TOut>(this TTarget target, Func<TTarget, TOut> valueFunc)
+            where TTarget : class
+        {
+            return target == null ? default(TOut) : valueFunc(target);
+        }
+
+        public static void CallIfNotNull<TTarget>(this TTarget target, Action<TTarget> actionToPerform)
+            where TTarget : class
+        {
+            if(target != null)
+            {
+                actionToPerform(target);   
+            }
+        }
+
+        public static T IfNotNull<T>(this object target, Func<T> valueFunc)
+            where T : class
+        {
+            return target == null ? null : valueFunc();
+        }
     }
 }
