9a59c6bf512908da23d5a38c36543cd939b1a10a
Jeremy Miller <jeremydmiller@yahoo.com>
2011-10-17 15:05:54 -0500

    cleaned up GetStringRequest

---
+++ src/FubuCore.Testing/GetStringRequestTester.cs
@@ -0,0 +1,38 @@
+using FubuCore.Reflection;
+using FubuCore.Testing.Reflection.Expressions;
+using Microsoft.Practices.ServiceLocation;
+using NUnit.Framework;
+using Rhino.Mocks;
+using FubuTestingSupport;
+
+namespace FubuCore.Testing
+{
+    [TestFixture]
+    public class GetStringRequestTester
+    {
+        [Test]
+        public void when_creating_a_string_request_with_services_via_constructor()
+        {
+            var locator = MockRepository.GenerateMock<IServiceLocator>();
+            var accessor = ReflectionHelper.GetAccessor<Case>(x => x.Contact.FirstName);
+
+            var request = new GetStringRequest(accessor, "something", locator);
+
+            request.PropertyType.ShouldEqual(typeof (string));
+            request.OwnerType.ShouldEqual(typeof (Contact));
+
+
+        }
+
+        [Test]
+        public void property_type_is_determined_from_the_prop_if_no_accessor()
+        {
+            var property = ReflectionHelper.GetProperty<Case>(x => x.Identifier);
+            var request = new GetStringRequest(property, "something");
+
+            request.OwnerType.ShouldEqual(typeof (Case));
+            request.PropertyType.ShouldEqual(typeof (string));
+
+        }
+    }
+}
