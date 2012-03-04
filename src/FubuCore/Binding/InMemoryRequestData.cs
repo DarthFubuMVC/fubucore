19929f46b0aa74c3963b6f9b46ddd94f54a00457
Jeremy Miller <jeremydmiller@yahoo.com>
2012-03-04 15:20:26 -0600

    removed RequestDataBase

--- src/FubuCore/Binding/InMemoryRequestData.cs
+++ src/FubuCore/Binding/InMemoryRequestData.cs
@@ -1,10 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using FubuCore.Util;
 
 namespace FubuCore.Binding
 {
-    public class InMemoryRequestData : RequestDataBase
+    public class InMemoryRequestData : IRequestData
     {
         private readonly Cache<string, object> _values = new Cache<string, object>();
 
@@ -17,35 +18,56 @@ namespace FubuCore.Binding
             _values = new Cache<string, object>(values);
         }
 
+        public object Value(string key)
+        {
+            return _values.Has(key) ? _values[key] : null;
+        }
+
+        public bool Value(string key, Action<BindingValue> callback)
+        {
+            if (_values.Has(key))
+            {
+                callback(new BindingValue
+                {
+                    RawKey = key,
+                    RawValue = _values[key],
+                    Source = "in memory"
+                });
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool HasChildRequest(string key)
+        {
+            return _values.GetAllKeys().Any(x => x.StartsWith(key));
+        }
+
+        public IRequestData GetChildRequest(string prefixOrChild)
+        {
+            return new PrefixedRequestData(this, prefixOrChild);
+        }
+
+        public IEnumerable<IRequestData> GetEnumerableRequests(string prefixOrChild)
+        {
+            return EnumerateFlatRequestData.For(this, prefixOrChild);
+        }
+
         public object this[string key]
         {
             get { return _values[key]; }
             set { _values[key] = value; }
         }
 
-        protected override object Get(string key)
-        {
-            return _values[key];
-        }
-
-        protected override bool Has(string key)
-        {
-            return _values.Has(key);
-        }
-
-        public override IEnumerable<string> GetKeys()
-        {
-            return _values.GetAllKeys();
-        }
-
-        protected override string source
-        {
-            get { return "in memory"; }
-        }
-
         public void ReadData(string text)
         {
             StringPropertyReader.ForText(text).ReadProperties((key, value) => _values[key] = value);
+        }
+
+        public IEnumerable<string> GetKeys()
+        {
+            return _values.GetAllKeys();
         }
     }
 }
--- src/FubuCore/Configuration/AppSettingsRequestData.cs
+++ src/FubuCore/Configuration/AppSettingsRequestData.cs
@@ -1,10 +1,9 @@
 using System;
 using System.Linq.Expressions;
-using FubuCore.Binding;
 
 namespace FubuCore.Configuration
 {
-    public class AppSettingsRequestData : InMemoryRequestData
+    public class AppSettingsRequestData
     {
         [Obsolete("Use the value on AppSettingsProvider instead")]
         public static string GetValueFor<T>(Expression<Func<T, object>> property)
