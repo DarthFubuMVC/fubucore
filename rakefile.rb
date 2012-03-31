f6d2a20a5414b5ff08f95432a41bb8fce8f11443
Jeremy Miller <jeremydmiller@yahoo.com>
2012-03-31 07:31:27 -0500

    updated the RakeFile so that it would not auto-publish nugets

--- rakefile.rb
+++ rakefile.rb
@@ -26,7 +26,7 @@ desc "**Default**, compiles and runs tests"
 task :default => [:compile, :unit_test]
 
 desc "Target used for the CI server"
-task :ci => [:update_all_dependencies, :default, :history, :publish]
+task :ci => [:update_all_dependencies, :default, :history]
 
 
 
