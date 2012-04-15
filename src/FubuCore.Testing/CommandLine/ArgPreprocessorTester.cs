e6e5864139e6e07e07f36db87f0963b3ca2d62d6
Chad Myers <chad.myers@gmail.com>
2012-04-14 23:51:59 -0500

    Add ArgPreprocessor to sort out multi-args

---
+++ src/FubuCore.Testing/CommandLine/ArgPreprocessorTester.cs
@@ -0,0 +1,34 @@
+﻿using FubuCore.CommandLine;
+using NUnit.Framework;
+using FubuTestingSupport;
+
+namespace FubuCore.Testing.CommandLine
+{
+    [TestFixture]
+    public class ArgPreprocessorTester
+    {
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [Test]
+        public void should_split_multi_args()
+        {
+            ArgPreprocessor.Process(new[] {"-abc"}).ShouldHaveTheSameElementsAs("-a", "-b", "-c");
+        }
+
+        [Test]
+        public void should_ignore_long_flag_args()
+        {
+            ArgPreprocessor.Process(new[] {"--abc"}).ShouldHaveTheSameElementsAs("--abc");
+        }
+
+        [Test]
+        public void should_support_multiple_types_of_flags()
+        {
+            ArgPreprocessor.Process(new[] { "-abc", "--xyz", "b" }).ShouldHaveTheSameElementsAs("-a", "-b", "-c", "--xyz", "b");
+        }
+
+    }
+}
