dfef32c2fc1bb1d13c42dd808396011a49f98fd4
Chad Myers <chad.myers@gmail.com>
2012-04-14 22:27:57 -0500

    Update FlagAliasAttribute to take long and short forms
    Obsolete the single-arg c'tor to force people to provide both forms

--- src/FubuCore/CommandLine/FlagAliasAttribute.cs
+++ src/FubuCore/CommandLine/FlagAliasAttribute.cs
@@ -6,15 +6,28 @@ namespace FubuCore.CommandLine
     public class FlagAliasAttribute : Attribute
     {
         private readonly string _alias;
+        private readonly char _oneLetterAlias;
 
+        [Obsolete("Must provide long (--flag) and short (-f) form when aliasing a flag. Use other FlagAliasAttribute constructor")]
         public FlagAliasAttribute(string alias)
+            : this(alias, alias[0])
+        {
+        }
+
+        public FlagAliasAttribute(string alias, char oneLetterAlias)
         {
             _alias = alias;
+            _oneLetterAlias = oneLetterAlias;
         }
 
         public string Alias
         {
             get { return _alias; }
+        }
+
+        public char OneLetterAlias
+        {
+            get { return _oneLetterAlias; }
         }
     }
 }
