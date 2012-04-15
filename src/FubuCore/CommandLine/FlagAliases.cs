cecd6b3d0cbaeb1576b3038a41b6e897ff26c645
Chad Myers <chad.myers@gmail.com>
2012-04-14 22:27:22 -0500

    Add FlagAliases class to represent the multiple aliases for a flag

---
+++ src/FubuCore/CommandLine/FlagAliases.cs
@@ -0,0 +1,20 @@
+﻿namespace FubuCore.CommandLine
+{
+    public class FlagAliases
+    {
+        public string LongForm { get; set; }
+        public string ShortForm { get; set; }
+
+        public bool Matches(string token)
+        {
+            var lowerToken = token.ToLower();
+
+            return lowerToken == ShortForm.ToLower() || lowerToken == LongForm.ToLower();
+        }
+
+        public override string ToString()
+        {
+            return "{0}, {1}".ToFormat(ShortForm, LongForm);
+        }
+    }
+}
