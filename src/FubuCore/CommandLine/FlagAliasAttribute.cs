d90188bf4ee232d2b4cc59ada55724ff495b4d13
Ryan Rauh <rauh.ryan@gmail.com>
2012-04-25 15:41:42 -0500

    making ShortForm alias flags case sensitive, fixes #34

--- src/FubuCore/CommandLine/FlagAliasAttribute.cs
+++ src/FubuCore/CommandLine/FlagAliasAttribute.cs
@@ -6,7 +6,7 @@ namespace FubuCore.CommandLine
     public class FlagAliasAttribute : Attribute
     {
         private readonly string _longAlias;
-        private readonly char _oneLetterAlias;
+        private readonly char? _oneLetterAlias;
 
         public FlagAliasAttribute(string longAlias, char oneLetterAlias)
         {
@@ -14,12 +14,22 @@ namespace FubuCore.CommandLine
             _oneLetterAlias = oneLetterAlias;
         }
 
+        public FlagAliasAttribute(char oneLetterAlias)
+        {
+            _oneLetterAlias = oneLetterAlias;
+        }
+
+        public FlagAliasAttribute(string longAlias)
+        {
+            _longAlias = longAlias;
+        }
+
         public string LongAlias
         {
             get { return _longAlias; }
         }
 
-        public char OneLetterAlias
+        public char? OneLetterAlias
         {
             get { return _oneLetterAlias; }
         }
