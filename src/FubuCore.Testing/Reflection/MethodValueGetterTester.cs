a524176fca68721b17147e1e82afb10feba0720d
GaryLCoxJr <gary.l.coxjr@gmail.com>
2013-07-12 13:01:03 -0500

    Added test for GetHashCode on MethodValueGetter DarthFubuMVC/fubucore#74

---
+++ src/FubuCore.Testing/Reflection/MethodValueGetterTester.cs
@@ -0,0 +1,25 @@
+﻿using FubuCore.Reflection;
+using FubuTestingSupport;
+using NUnit.Framework;
+
+namespace FubuCore.Testing.Reflection
+{
+    [TestFixture]
+    public class MethodValueGetterTester
+    {
+        [Test]
+        public void should_get_hashcode()
+        {
+            var accessor = ReflectionHelper.GetAccessor<TestSubject>(x => x.Value());
+            accessor.GetHashCode().ShouldNotBeNull();
+        }
+    }
+
+    public class TestSubject
+    {
+        public object Value()
+        {
+            return new object();
+        }
+    }
+}
