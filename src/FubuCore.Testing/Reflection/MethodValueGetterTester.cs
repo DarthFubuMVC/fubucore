2612dff08493bed9c7fe8a11a26009aaf3969c73
GaryLCoxJr <gary.l.coxjr@gmail.com>
2013-07-15 12:57:54 -0500

    Added more direct tests against GetHashCode, ref #74

--- src/FubuCore.Testing/Reflection/MethodValueGetterTester.cs
+++ src/FubuCore.Testing/Reflection/MethodValueGetterTester.cs
@@ -1,4 +1,5 @@
-﻿using FubuCore.Reflection;
+﻿using System.Reflection;
+using FubuCore.Reflection;
 using FubuTestingSupport;
 using NUnit.Framework;
 
@@ -7,11 +8,36 @@ namespace FubuCore.Testing.Reflection
     [TestFixture]
     public class MethodValueGetterTester
     {
+        private readonly MethodInfo TheMethodInfo = ReflectionHelper.GetMethod<TestSubject>(x => x.Value());
+        private readonly Accessor TheAccessor = ReflectionHelper.GetAccessor<TestSubject>(x => x.Value());
+        private readonly Accessor TheArgAccessor = ReflectionHelper.GetAccessor<TestSubject>(x => x.AnotherMethod("Test"));
+        private readonly MethodInfo TheArgMethodInfo = ReflectionHelper.GetMethod<TestSubject>(x => x.AnotherMethod("Test"));
+
         [Test]
-        public void should_get_hashcode()
+        public void should_not_have_null_hashcode()
         {
-            var accessor = ReflectionHelper.GetAccessor<TestSubject>(x => x.Value());
-            accessor.GetHashCode().ShouldNotBeNull();
+            TheAccessor.GetHashCode().ShouldNotBeNull();
+        }
+
+        [Test]
+        public void hashcode_should_not_eq_zero()
+        {
+            TheAccessor.GetHashCode().ShouldNotEqual(0);
+        }
+
+        [Test]
+        public void should_return_methodinfo_hash()
+        {
+            var expectedHash = TheMethodInfo.GetHashCode();
+            TheAccessor.GetHashCode().ShouldEqual(expectedHash);
+        }
+
+        [Test]
+        public void with_arguments_should_get_correct_hashcode()
+        {
+            var actual = TheArgAccessor.GetHashCode();
+            var expectedHash = (TheArgMethodInfo.GetHashCode() * 397) ^ ("Test".GetHashCode());
+            actual.ShouldEqual(expectedHash);
         }
     }
 
@@ -20,6 +46,11 @@ namespace FubuCore.Testing.Reflection
         public object Value()
         {
             return new object();
+        }
+
+        public object AnotherMethod(string arg1)
+        {
+            return arg1;
         }
     }
 }
