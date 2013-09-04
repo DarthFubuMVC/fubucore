9a1eb055c3c6c53a7cb8379242f1106e176ade95
jeremydmiller <jeremydmiller@yahoo.com>
2013-09-04 07:34:51 -0500

    IFileSystem.ForceClean().  Closes GH-73

--- src/FubuCore/FileSystemExtensions.cs
+++ src/FubuCore/FileSystemExtensions.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using FubuCore.CommandLine;
 
 namespace FubuCore
@@ -138,6 +139,55 @@ namespace FubuCore
         public static string SearchUpForDirectory(this IFileSystem fileSystem, string directoryToFind)
         {
             return fileSystem.SearchUpForDirectory(".", directoryToFind);
+        }
+
+        // Basic integration coverage on this but having to rely mostly on manual testing here
+        /// <summary>
+        /// Does a "smart" cleanup of the contents of a folder by finding every file
+        /// and deleting one file at a time from longest path to shortest.  Can get around
+        /// file locking issues with a straight up IFileSystem.CleanDirectory()
+        /// </summary>
+        /// <param name="system"></param>
+        /// <param name="path"></param>
+        public static void ForceClean(this IFileSystem system, string path)
+        {
+            if (path.IsEmpty()) return;
+            if (!Directory.Exists(path)) return;
+
+            try
+            {
+                cleanDirectory(path, false);
+            }
+            catch
+            {
+                // just retry it
+                cleanDirectory(path, false);
+            }
+        }
+
+        private static void cleanDirectory(string directory, bool remove = true)
+        {
+            string[] files = Directory.GetFiles(directory);
+            string[] children = Directory.GetDirectories(directory);
+
+            foreach (var file in files)
+            {
+                File.SetAttributes(file, FileAttributes.Normal);
+                File.Delete(file);
+            }
+
+            Thread.Sleep(10);
+
+            foreach (var child in children)
+            {
+                cleanDirectory(child);
+            }
+
+            if (remove)
+            {
+                Thread.Sleep(10);
+                Directory.Delete(directory, false);
+            }
         }
     }
 }
