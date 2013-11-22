d3e112503ed7904dde0e5e4a2a626f1b9dcd97a6
Sam Merrell <merrell.sam@gmail.com>
2013-11-22 15:16:22 -0700

    Fix whitespace in TemplateParserTester
    Mixed tabs and spaces got switched to just spaces.

--- src/FubuCore.Testing/TemplateParserTester.cs
+++ src/FubuCore.Testing/TemplateParserTester.cs
@@ -7,28 +7,28 @@ namespace FubuCore.Testing
     [TestFixture]
     public class TemplateParserTester
     {
-		[Test]
-		public void should_replace_a_single_variable()
-		{
-			var template = "this is a {test} template";
-			var substitutions = new Dictionary<string, string>
-                                    {
-                                        {"test", "replaced"},
-                                    };
+        [Test]
+        public void should_replace_a_single_variable()
+        {
+            var template = "this is a {test} template";
+            var substitutions = new Dictionary<string, string>
+            {
+                {"test", "replaced"},
+            };
 
-			TemplateParser
-				.Parse(template, substitutions)
-				.ShouldEqual("this is a replaced template");
-		}
+            TemplateParser
+                .Parse(template, substitutions)
+                .ShouldEqual("this is a replaced template");
+        }
 
         [Test]
         public void should_replace_a_single_variable_with_a_dash()
         {
             var template = "this is a {test-name} template";
             var substitutions = new Dictionary<string, string>
-                                    {
-                                        {"test-name", "replaced"},
-                                    };
+            {
+                {"test-name", "replaced"},
+            };
 
             TemplateParser
                 .Parse(template, substitutions)
@@ -41,9 +41,9 @@ namespace FubuCore.Testing
         {
             var template = "*{db}*";
             var substitutions = new Dictionary<string, string>
-                                    {
-                                        {"db", "blue"},
-                                    };
+            {
+                {"db", "blue"},
+            };
 
             TemplateParser.Parse(template, substitutions).ShouldEqual("*blue*");
         }
@@ -53,44 +53,44 @@ namespace FubuCore.Testing
         {
             var template = "**{db}";
             var substitutions = new Dictionary<string, string>
-                                    {
-                                        {"db", "blue"},
-                                    };
+            {
+                {"db", "blue"},
+            };
 
             TemplateParser.Parse(template, substitutions).ShouldEqual("**blue");
         }
 
-    	[Test]
+        [Test]
         public void should_replace_multiple_variables()
         {
             var template = "this {is} a {test} template with {a} few {variables}";
             var substitutions = new Dictionary<string, string>
-                                    {
-                                        {"is", "is"},
-                                        {"test", "replaced"},
-                                        {"a", "more than a"},
-                                        {"variables", "witty tricks."}
-                                    };
+            {
+                {"is", "is"},
+                {"test", "replaced"},
+                {"a", "more than a"},
+                {"variables", "witty tricks."}
+            };
 
             TemplateParser
                 .Parse(template, substitutions)
                 .ShouldEqual("this is a replaced template with more than a few witty tricks.");
         }
 
-		[Test]
-		public void should_replace_values_when_template_begins_with_a_variable()
-		{
-			var template = "{that} is a {test} template";
-			var substitutions = new Dictionary<string, string>
-                                    {
-										{"that", "this"},
-                                        {"test", "replaced"},
-                                    };
+        [Test]
+        public void should_replace_values_when_template_begins_with_a_variable()
+        {
+            var template = "{that} is a {test} template";
+            var substitutions = new Dictionary<string, string>
+            {
+                {"that", "this"},
+                {"test", "replaced"},
+            };
 
-			TemplateParser
-				.Parse(template, substitutions)
-				.ShouldEqual("this is a replaced template");
-		}
+            TemplateParser
+                .Parse(template, substitutions)
+                .ShouldEqual("this is a replaced template");
+        }
 
         [Test]
         public void get_substitutions()
