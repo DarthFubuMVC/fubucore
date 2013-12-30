e9ddacf3e9bcaab8a2ddbd90fb0ed50ac1e1488a
Jeremy D. Miller <jeremydmiller@yahoo.com>
2013-12-30 12:56:15 -0600

    Removing the error handling in rake that doesn't work anyway

--- rakefile.rb
+++ rakefile.rb
@@ -1,13 +1,4 @@
-begin
-  require 'bundler/setup'
-  require 'fuburake'
-rescue LoadError
-  puts 'Bundler and all the gems need to be installed prior to running this rake script. Installing...'
-  system("gem install bundler --source http://rubygems.org")
-  sh 'bundle install'
-  system("bundle exec rake", *ARGV)
-  exit 0
-end
+require 'fuburake'
 
 
 FubuRake::Solution.new do |sln|
