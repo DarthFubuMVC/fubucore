b09243852378ac0757a7799c77bcb3f84c3954ed
andy <andy@andydote.co.uk>
2014-06-26 17:34:20 +0100

    commandLine: add small abstraction for command creation

---
+++ src/FubuCore/CommandLine/ActivatorCommandCreator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FubuCore.CommandLine
+{
+    public class ActivatorCommandCreator : ICommandCreator
+    {
+        public IFubuCommand Create(Type commandType)
+        {
+            return Activator.CreateInstance(commandType).As<IFubuCommand>();
+        }
+    }
+}
---
+++ src/FubuCore/CommandLine/ICommandCreator.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace FubuCore.CommandLine
+{
+    public interface ICommandCreator
+    {
+        IFubuCommand Create(Type commandType);
+    }
+}
