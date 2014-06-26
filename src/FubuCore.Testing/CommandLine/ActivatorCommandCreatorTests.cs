9e9d2e195e58a50890803b0e8347c51921de9190
andy <andy@andydote.co.uk>
2014-06-26 17:36:08 +0100

    commandline.Tests: add activatorCommandCreatorTests

---
+++ src/FubuCore.Testing/CommandLine/ActivatorCommandCreatorTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using FubuCore.CommandLine;
+using FubuTestingSupport;
+using NUnit.Framework;
+
+namespace FubuCore.Testing.CommandLine
+{
+    public class ActivatorCommandCreatorTests
+    {
+        [Test]
+        public void builds_an_instance_with_no_ctor_parameters()
+        {
+            var creator = new ActivatorCommandCreator();
+            var instance = creator.Create(typeof (NoParamsCommand));
+
+            instance.ShouldBeOfType<NoParamsCommand>();
+        }
+
+        [Test]
+        public void throws_if_the_ctor_has_parameters()
+        {
+            var creator = new ActivatorCommandCreator();
+
+            Assert.Throws<MissingMethodException>(() => creator.Create(typeof (ParamsCommand)));
+        }
+
+        public class FakeModel
+        {
+        }
+
+        private class NoParamsCommand : FubuCommand<FakeModel>
+        {
+            public override bool Execute(FakeModel input)
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
+        private class ParamsCommand : FubuCommand<FakeModel>
+        {
+            public ParamsCommand(string testArgument)
+            {
+            }
+
+            public override bool Execute(FakeModel input)
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+    }
+}
