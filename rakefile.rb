d0b2b1d96531972c7e3d8b00345a950be967b03c
Jeremy D. Miller <jeremydmiller@yahoo.com>
2015-03-24 08:33:27 -0500

    Just eliminating some defaults for a better demo;)

--- rakefile.rb
+++ rakefile.rb
@@ -6,9 +6,6 @@ FubuRake::Solution.new do |sln|
 		:product_name => "FubuCore",
 		:copyright => 'Copyright 2008-2013 Jeremy D. Miller, Josh Arnold, Joshua Flanagan, et al. All rights reserved.'
 	}
-	
-	sln.ripple_enabled = true
-	sln.fubudocs_enabled = true
 end
 
 
