8372180eefa3b8eb966242dfac1ce65da470c05a
Jeremy D. Miller <jeremydmiller@yahoo.com>
2015-03-24 08:33:45 -0500

    Resetting the copyright

--- rakefile.rb
+++ rakefile.rb
@@ -4,7 +4,7 @@ require 'fuburake'
 FubuRake::Solution.new do |sln|
 	sln.assembly_info = {
 		:product_name => "FubuCore",
-		:copyright => 'Copyright 2008-2013 Jeremy D. Miller, Josh Arnold, Joshua Flanagan, et al. All rights reserved.'
+		:copyright => 'Copyright 2008-2015 Jeremy D. Miller, Josh Arnold, Joshua Flanagan, et al. All rights reserved.'
 	}
 end
 
